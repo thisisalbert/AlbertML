30223a7edbaa736217bedfa6d8edfbf4ec5a5197
Albert Garcia Lopez <65236292+thisisalbert@users.noreply.github.com>
2022-04-29 15:55:21 +0200

    Update unregisterCluster.R

--- unregisterCluster.R
+++ unregisterCluster.R
@@ -1,12 +1,6 @@
 unregisterCluster <- function() {
-  
-  if (any(stringr::str_detect(ls(), "cl"))) {
-    parallel::stopCluster(cl)
-    env <- foreach:::.foreachGlobals
-    rm(list = ls(name = env), pos = env)
-    gc()
-  } else {
-    warning("Caution: No cluster object is defined in the current environment!")
-  }
-  
+  parallel::stopCluster(cl)
+  env <- foreach:::.foreachGlobals
+  rm(list = ls(name = env), pos = env)
+  gc()
 }
