28cd9b904a62ec9ef98bb0bcfd08f62b1c9832ad
Albert Garcia Lopez <65236292+thisisalbert@users.noreply.github.com>
2022-05-04 20:25:33 +0200

    Update makeCVseeds.R

--- makeCVseeds.R
+++ makeCVseeds.R
@@ -1,12 +1,31 @@
-makeCVseeds <- function(tuneLength = NULL, numCV = 10, repeatsCV = NULL) {
+makeCVseeds <- function(
+    tunes = NULL, numCV = 10, repeatsCV = NULL, type = c("tuneGrid", "tuneLength")
+) {
   
-  c(
-    replicate(
-      expr = sample.int(n = 2^15, size = tuneLength, replace = FALSE),
-      n = ifelse(is.null(repeatsCV), numCV, repeatsCV * numCV),
-      simplify = FALSE
-    ),
-    sample.int(n = 2^15, size = 1, replace = FALSE)
-  )  
+  # When tuneLength is used
+  
+  if (type == "tuneLength") {
+    c(
+      replicate(
+        expr = sample.int(n = 2^15, size = tunes, replace = FALSE),
+        n = ifelse(is.null(repeatsCV), numCV, repeatsCV * numCV),
+        simplify = FALSE
+      ),
+      sample.int(n = 2^15, size = 1, replace = FALSE)
+    )  
+  }
+  
+  # When tuneGrid is used
+  
+  if (type == "tuneGrid") {
+    c(
+      replicate(
+        expr = sample.int(n = 2^15, size = 1, replace = FALSE),
+        n = ifelse(is.null(repeatsCV), numCV, repeatsCV * numCV),
+        simplify = FALSE
+      ),
+      sample.int(n = 2^15, size = 1, replace = FALSE)
+    )  
+  }
   
 }
