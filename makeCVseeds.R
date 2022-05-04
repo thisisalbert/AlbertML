580167e8891fd0eddfd43d05695210f75927e48f
Albert Garcia Lopez <65236292+thisisalbert@users.noreply.github.com>
2022-05-04 17:43:40 +0200

    Update makeCVseeds.R

--- makeCVseeds.R
+++ makeCVseeds.R
@@ -1,12 +1,12 @@
-makeCVseeds <- function(tuneLength = NULL, numCVs = 10, repeatsCV = NULL) {
+makeCVseeds <- function(tuneLength = NULL, numCV = 10, repeatsCV = NULL) {
   
   c(
     replicate(
       expr = sample.int(n = 2^15, size = tuneLength, replace = FALSE),
-      n = if_else(repeatsCV == NULL, numCVs, repeatsCV * numCVs),
+      n = ifelse(is.null(repeatsCV), numCV, repeatsCV * numCV),
       simplify = FALSE
     ),
     sample.int(n = 2^15, size = 1, replace = FALSE)
-  )
+  )  
   
 }
