b35294b383d4f1c551424632c6e984671e2e4786
Albert Garcia Lopez <65236292+thisisalbert@users.noreply.github.com>
2022-05-13 16:22:22 +0200

    Create getTestResults_MultiClass.R

---
+++ getTestResults_MultiClass.R
@@ -0,0 +1,78 @@
+getTestResults_MultiClass <- function(case = case, control = "The rest", actuals, predictedScores, threshold = 0.5) {
+
+  cm = InformationValue::confusionMatrix(
+    actuals = actuals,
+    predictedScores = predictedScores,
+    threshold = threshold
+  ) %>%
+    as.data.frame() %>%
+    rownames_to_column("predicted") %>%
+    pivot_longer(cols = c("0", "1"), names_to = "actuals", values_to = "freq") %>%
+    mutate(across(predicted:actuals, ~ case_when(
+      .x == "0" ~ control,
+      .x == "1" ~ case
+    ))) %>%
+    mutate(across(predicted:actuals, ~ factor(.x, levels = c(case, control))))
+
+  auroc = PRROC::roc.curve(
+    scores.class0 = predictedScores,
+    weights.class0 = actuals,
+    curve = TRUE
+  )
+
+  metrics = bind_cols(
+    auroc %>%
+      chuck("auc") %>%
+      tibble(ROC = .),
+    cm %>%
+      filter(predicted == actuals) %>%
+      pull(freq) %>%
+      sum() %>%
+      magrittr::divide_by(sum(cm$freq)) %>%
+      tibble(Accuracy = .),
+    InformationValue::kappaCohen(
+      actuals = actuals,
+      predictedScores = predictedScores,
+      threshold = threshold
+    ) %>%
+      magrittr::divide_by(100) %>%
+      tibble(Kappa = .),
+    InformationValue::precision(
+      actuals = actuals,
+      predictedScores = predictedScores,
+      threshold = threshold
+    ) %>%
+      tibble(PPV = .),
+    InformationValue::npv(
+      actuals = actuals,
+      predictedScores = predictedScores,
+      threshold = threshold
+    ) %>%
+      tibble(NPV = .),
+    InformationValue::sensitivity(
+      actuals = actuals,
+      predictedScores = predictedScores,
+      threshold = threshold
+    ) %>%
+      tibble(Sens = .),
+    InformationValue::specificity(
+      actuals = actuals,
+      predictedScores = predictedScores,
+      threshold = threshold
+    ) %>%
+      tibble(Spec = .)
+  ) %>%
+    mutate(F1 = (2 * PPV * Sens)/(PPV + Sens)) %>%
+    select(Accuracy, Kappa, ROC, Sens, Spec, PPV, NPV, F1) %>%
+    mutate(Type = "Test", Case = case, .before = everything())
+
+  return(
+    list(
+      case = case,
+      cm = cm,
+      auroc = auroc,
+      metrics = metrics
+    )
+  )
+
+}
