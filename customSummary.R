de254c7d3ed2c507192b86ac53cd690ebf771c35
Albert Garcia Lopez <65236292+thisisalbert@users.noreply.github.com>
2022-05-23 19:06:12 +0200

    Update customSummary.R

--- customSummary.R
+++ customSummary.R
@@ -1,13 +1,16 @@
-customSummary <- function(data, lev = NULL, model = NULL){
-
+customFunction <- function(data, lev = NULL, model = NULL){
+  
   default <- caret::defaultSummary(data, lev, model)
   twoclass <- caret::twoClassSummary(data, lev, model)
+  bal_acc <- (twoclass[["Sens"]] + twoclass[["Spec"]]) / 2
+  names(bal_acc) <- "Bal_Accuracy"
   ppv <- caret::posPredValue(data[, "pred"], data[, "obs"])
   names(ppv) <- "PPV"
   npv <- caret::negPredValue(data[, "pred"], data[, "obs"])
   names(npv) <- "NPV"
   f_score <- caret::F_meas(data[, "pred"], data[, "obs"])
   names(f_score) <- "F1"
-  return(c(default, twoclass, ppv, npv, f_score))
+  
+  return(c(default, bal_acc, twoclass, ppv, npv, f_score))
   
 }
