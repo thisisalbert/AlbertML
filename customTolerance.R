ec2e35b132026e1ba925e14bc3ce316ec609b588
Albert Garcia Lopez <65236292+thisisalbert@users.noreply.github.com>
2022-05-24 19:02:47 +0200

    Create customTolerance.R

---
+++ customTolerance.R
@@ -0,0 +1,19 @@
+function(x, metric, tol = 5, maximize) {
+
+  index <- 1:nrow(x)
+  
+  if (!maximize) {
+    best <- min(x[, metric])
+    perf <- (x[, metric] - best)/best * 100
+    candidates <- index[perf < tol]
+    bestIter <- min(candidates)
+  } else {
+    best <- max(x[, metric])
+    perf <- (x[, metric] - best)/best * -100
+    candidates <- index[perf < tol]
+    bestIter <- min(candidates)
+  }
+  
+  bestIter
+  
+}
