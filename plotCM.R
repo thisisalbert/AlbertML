4ab0e87f43696a3075ddce65c222bd8cc8d318f7
Albert Garcia Lopez <65236292+thisisalbert@users.noreply.github.com>
2022-06-15 19:17:42 +0200

    Create plotCM.R

---
+++ plotCM.R
@@ -0,0 +1,24 @@
+plotCM <- function(df, title, subtitle, color) {
+  df %>% 
+    mutate(Status = ifelse(Prediction == Reference, "Hit", "Miss")) %>% 
+    ggplot(aes(
+      x = Reference, y = Prediction, label = Freq, fill = Status
+    )) +
+    geom_tile() +
+    geom_text(fontface = "bold", size = 12, vjust = 0.5, hjust = 0.5) +
+    scale_x_discrete(position = "top", expand = c(0,0)) +
+    scale_y_discrete(limits = rev, expand = c(0,0)) +
+    scale_fill_manual(values = c("Miss" = "white", "Hit" = color)) +
+    coord_fixed() +
+    labs(title = title, subtitle = subtitle) +
+    theme(
+      text = element_text(size = 20),
+      axis.title = element_text(face = "bold"),
+      axis.ticks = element_blank(),
+      legend.position = "none",
+      panel.background = element_blank(),
+      plot.title = element_text(face = "bold"),
+      plot.subtitle = element_text(face = "italic"),
+      panel.border = element_rect(color = color, fill = NA, size = 2)
+    )
+}
