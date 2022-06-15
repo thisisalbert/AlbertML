f38db437c160af7aa908bbf371e29eb2786e429a
Albert Garcia Lopez <65236292+thisisalbert@users.noreply.github.com>
2022-06-15 19:05:46 +0200

    Update plotPCABiplot.R

--- plotPCABiplot.R
+++ plotPCABiplot.R
@@ -1,24 +1,30 @@
-plotPCABiplot <- function(df, color, fill, var_pc1, var_pc2) {
+plotPCABiplot <- function(
+    df, color, fill, var_pc1, var_pc2, title = NULL, subtitle = NULL, 
+    nbreaks = 20, fontsize = 16
+) {
   df %>% 
     ggplot(
       aes_string(x = "PC1", y = "PC2", color = color, fill = fill), 
     ) +
     geom_point(alpha = 0.5) +
-    stat_ellipse(geom = "polygon", alpha = 0.1) +
-    scale_x_continuous(n.breaks = 25) +
-    scale_y_continuous(n.breaks = 25) +
-    geom_hline(yintercept = 0, lty = "dashed", color = "gray60") +
-    geom_vline(xintercept = 0, lty = "dashed", color = "gray60") +
+    stat_ellipse(geom = "polygon", alpha = 0.05) +
+    scale_x_continuous(n.breaks = nbreaks) +
+    scale_y_continuous(n.breaks = nbreaks) +
+    geom_hline(yintercept = 0, lty = "dashed", color = "black", alpha = 0.2) +
+    geom_vline(xintercept = 0, lty = "dashed", color = "black", alpha = 0.2) +
     labs(
-      x = paste0("PC1: ", round(var_pc1, 2), "% var. explained"),
-      y = paste0("PC2: ", round(var_pc2, 2), "% var. explained"),
+      x = paste0("PC1 (", round(var_pc1, 2), "% variance explained)"),
+      y = paste0("PC2 (", round(var_pc2, 2), "% variance explained)"),
     ) +
     paletteer::scale_color_paletteer_d("ggthemes::gdoc") +
     paletteer::scale_fill_paletteer_d("ggthemes::gdoc") +
+    labs(title = title, subtitle = subtitle) +
     theme_bw() +
     theme(
+      text = element_text(size = fontsize),
+      plot.title = element_text(face = "bold"),
       legend.title = element_blank(),
-      legend.position = "top",
+      legend.position = "bottom",
       legend.direction = "horizontal"
     )    
 }
