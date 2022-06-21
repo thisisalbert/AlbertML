d34f0aa822aa517358d752ba955074a70828be8d
Albert Garcia Lopez <65236292+thisisalbert@users.noreply.github.com>
2022-06-21 20:02:00 +0200

    Update getCM.R

--- getCM.R
+++ getCM.R
@@ -1,23 +1,51 @@
-getCM <- function(model, prob_preds, reference, case, control, type_opt = 0.5) {
-
-library(caret)
-library(InformationValue)
-
-threshold <- InformationValue::optimalCutoff(
-    actuals = ifelse(model$pred$obs == case, 1, 0),
-    predictedScores = model$pred[[case]],
-    optimiseFor = type_opt
-  )
+getCM <- function(model, prob_preds, real_labels, case, control, type_opt) {
   
-  custom_pred <- factor(
-    x = ifelse(prob_preds[[case]] > threshold, case, control), 
-    levels = c(case, control)
-  )
+  if (is.numeric(type_opt)) {
+    
+    custom_pred <- factor(
+      x = ifelse(prob_preds[[case]] > type_opt, case, control), 
+      levels = c(case, control)
+    )
+    
+    return(
+      c(
+        caret::confusionMatrix(
+          data = custom_pred,
+          reference = real_labels,
+          positive = case
+        ),
+        type_opt = type_opt
+      )
+    )
+    
+  }
   
-  caret::confusionMatrix(
-    data = custom_pred,
-    reference = reference,
-    positive = case
-  )
-
+  if (type_opt %in% c("misclasserror", "Both", "Ones", "Zeros")) {
+    
+    threshold <- InformationValue::optimalCutoff(
+      actuals = ifelse(model$pred$obs == case, 1, 0),
+      predictedScores = model$pred[[case]],
+      optimiseFor = type_opt,
+      returnDiagnostics = TRUE
+    )
+    
+    custom_pred <- factor(
+      x = ifelse(prob_preds[[case]] > threshold$optimalCutoff, case, control), 
+      levels = c(case, control)
+    )
+    
+    return(
+      c(
+        caret::confusionMatrix(
+          data = custom_pred,
+          reference = real_labels,
+          positive = case
+        ),
+        type_opt = type_opt
+        threshold
+      )
+    )
+    
+  }
+  
 }
