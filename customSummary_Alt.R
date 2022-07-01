cb2d3db2fbf5e1a5612a823cfb5d32f30530d3ce
Albert Garcia Lopez <65236292+thisisalbert@users.noreply.github.com>
2022-07-01 14:40:27 +0200

    Update customSummary_Alt.R

--- customSummary_Alt.R
+++ customSummary_Alt.R
@@ -4,6 +4,7 @@ customSummary_Alt <- function(data, lev = NULL, model = NULL) {
   twoclass = caret::twoClassSummary(data, lev, model)
   f_score = caret::F_meas(data[, "pred"], data[, "obs"]) %>% setNames("F1")
   bal_acc = (twoclass[["Sens"]] + twoclass[["Spec"]]) / 2 %>% setNames("Bal_Accuracy")
+  prevalence = seq(0.1, 0.9, 0.1)
   ppv = map(
       .x = prevalence,
       .f = ~ (twoclass[["Sens"]] * .x) / (twoclass[["Sens"]] * .x + (1 - twoclass[["Spec"]]) * (1 - .x))
