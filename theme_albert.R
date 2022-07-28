fde7f68803840fceaec4e74f251a9f4418021ede
Albert Garcia Lopez <65236292+thisisalbert@users.noreply.github.com>
2022-07-28 16:32:28 +0200

    Update theme_albert.R

--- theme_albert.R
+++ theme_albert.R
@@ -1,10 +1,10 @@
 theme_albert <- theme(
-  text = element_text(size = 14, color = "black"),
-  axis.text = element_text(size = 14, color = "grey40"),
+  text = element_text(size = ifelse(exists("font_size"), font_size, 14), color = "black"),
+  axis.text = element_text(size = ifelse(exists("font_size"), font_size, 14), color = "grey40"),
   panel.background = element_rect(fill = NA),
   panel.grid = element_line(color = "grey80"),
   panel.border = element_rect(fill = NA, size = 1.25),
-  plot.title = element_text(face = "bold"),
-  plot.subtitle = element_text(color = "grey40"),
+  plot.title = element_text(face = "bold", size = ifelse(exists("font_size"), font_size*1.2, 14*1.2)),
+  plot.subtitle = element_text(color = "grey40", size = ifelse(exists("font_size"), font_size*1.1, 14*1.1)),
   plot.caption = element_text(color = "grey40")
 )
