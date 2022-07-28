cbc00910a28e61074332515544734a6dff64754b
Albert Garcia Lopez <65236292+thisisalbert@users.noreply.github.com>
2022-07-28 16:29:13 +0200

    Create theme_albert.R
    ggplot theme used recurrently in my plots.

---
+++ theme_albert.R
@@ -0,0 +1,10 @@
+theme_albert <- theme(
+  text = element_text(size = 14, color = "black"),
+  axis.text = element_text(size = 14, color = "grey40"),
+  panel.background = element_rect(fill = NA),
+  panel.grid = element_line(color = "grey80"),
+  panel.border = element_rect(fill = NA, size = 1.25),
+  plot.title = element_text(face = "bold"),
+  plot.subtitle = element_text(color = "grey40"),
+  plot.caption = element_text(color = "grey40")
+)
