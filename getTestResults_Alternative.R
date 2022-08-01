5feb1f8b9c116054a02e224b15032206999b6afc
Albert Garcia Lopez <65236292+thisisalbert@users.noreply.github.com>
2022-08-01 19:10:08 +0200

    Update getTestResults_Alternative.R

--- getTestResults_Alternative.R
+++ getTestResults_Alternative.R
@@ -1,31 +1,29 @@
 getTestResults_Alternative <- function(
-	model, 
-	prob_preds, 
-	real_labels, 
-	case = case, 
-	control = control,
-	id_obs = id_obs,
-	type_opt = "misclasserror", 
-	type = "Test"
+    model, 
+    prob_preds, 
+    real_labels, 
+    id_obs,
+    case, 
+    type_opt = "misclasserror", 
+    type = "Test"
 ) {
-
-cm_obj = getCM(
-	model = model, 
-	prob_preds = prob_preds, 
-	real_labels = real_labels, 
-	id_obs = id_obs,
-	case = case, 
-	control = control, 
-	type_opt = type_opt
-)
-
-auroc_obj = PRROC::roc.curve(
+  
+  cm_obj = getCM(
+    model = model, 
+    prob_preds = prob_preds, 
+    real_labels = real_labels, 
+    id_obs = id_obs,
+    case = case, 
+    type_opt = type_opt
+  )
+  
+  auroc_obj = PRROC::roc.curve(
     scores.class0 = prob_preds[[case]],
     weights.class0 = ifelse(real_labels == case, 1, 0),
     curve = TRUE
   )
-	
-res <- list(CM = cm_obj, AUROC = auroc_obj)
-return(res)
-	
+  
+  res <- list(CM = cm_obj, AUROC = auroc_obj)
+  return(res)
+  
 }
