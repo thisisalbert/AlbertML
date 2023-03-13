4175af407e4c09e768fbdd409eda5ac32aab8733
Albert Garcia Lopez <65236292+thisisalbert@users.noreply.github.com>
2023-03-13 17:50:51 +0100

    Update saveWorkspace.R

--- saveWorkspace.R
+++ saveWorkspace.R
@@ -3,13 +3,13 @@ saveWorkspace <- function(workdir = outDir, saveEnv = TRUE, saveInfo = TRUE) {
   if (exists("workdir") == TRUE) {
     if (saveEnv) {
       save.image(
-        paste0(workdir, format(Sys.time(), "%y_%m_%d_workspace.RData"))
+        file.path(workdir, format(Sys.time(), "%y_%m_%d_workspace.RData"))
       )
     }
     if (saveInfo) {
       writeLines(
         text = capture.output(sessionInfo()), 
-        con = paste0(workdir, format(Sys.time(), "%y_%m_%d_sessionInfo.txt"))
+        con = file.path(workdir, format(Sys.time(), "%y_%m_%d_sessionInfo.txt"))
       )
     }
   }
@@ -17,13 +17,13 @@ saveWorkspace <- function(workdir = outDir, saveEnv = TRUE, saveInfo = TRUE) {
   if(exists("workdir") == FALSE) {
     if (saveEnv) {
       save.image(
-        paste0(format(Sys.time(), "%y_%m_%d_workspace.RData"))
+        file.path(format(Sys.time(), "%y_%m_%d_workspace.RData"))
       )
     }
     if (saveInfo) {
       writeLines(
         text = capture.output(sessionInfo()), 
-        con = paste0(format(Sys.time(), "%y_%m_%d_sessionInfo.txt"))
+        con = file.path(format(Sys.time(), "%y_%m_%d_sessionInfo.txt"))
       )
     }
   }
