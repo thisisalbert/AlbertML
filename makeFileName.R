12d970860bcf0f7779c28989d4afda7b54ff5cb8
Albert Garcia Lopez <65236292+thisisalbert@users.noreply.github.com>
2023-03-13 17:49:43 +0100

    Update makeFileName.R

--- makeFileName.R
+++ makeFileName.R
@@ -1,6 +1,6 @@
 makeFileName <- function(file_name, dir = outDir) {
   if (exists(deparse(substitute(dir)))) {
-    paste0(dir, paste0(format(Sys.time(), "%y_%m_%d_"), file_name))
+    file.path(dir, paste0(format(Sys.time(), "%y_%m_%d_"), file_name))
   } else {
     file.path(getwd(), paste0(format(Sys.time(), "%y_%m_%d_"), file_name))
   }
