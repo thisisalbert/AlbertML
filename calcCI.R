48e2e1615054acb61fe3e8f8e916a86ec738b276
AGarcia <albert.garcia.lopez@hki-jena.de>
2023-04-20 17:59:06 +0200

    Added a 'R' dedicated folder to keep all R scripts in

--- calcCI.R
+++
@@ -1,8 +0,0 @@
-calcCI <- function(x, perc = 0.95) {
-  x %>%
-    t.test(conf.level = perc) %>%
-    chuck("conf.int") %>%
-    as.numeric() %>%
-    formatC(digits = 3, format = "f") %>%
-    paste(collapse = "-")
-}
--- makeFileName.R
+++
@@ -1,7 +0,0 @@
-makeFileName <- function(file_name, dir = outDir) {
-  if (exists(deparse(substitute(dir)))) {
-    file.path(dir, paste0(format(Sys.time(), "%y_%m_%d_"), file_name))
-  } else {
-    file.path(getwd(), paste0(format(Sys.time(), "%y_%m_%d_"), file_name))
-  }
-}
