8c6b8d51b776282c408b2ff3c7371ed0142eb1e0
Albert Garcia Lopez <albertgarcialopez313@gmail.com>
2023-05-17 19:26:14 +0200

    Update extendedSummary.R

--- LEGACY/extendedSummary.R
+++ LEGACY/extendedSummary.R
@@ -1,1 +1,21 @@
+extendedSummary <- function(data, lev = NULL, model = NULL) {
+  
+  ROC = as.numeric(pROC::roc(data$obs, data[, lev[1]], direction = ">", quiet = TRUE)$auc)
+  Sensitivity = caret::sensitivity(data[, "pred"], data[, "obs"])
+  Specificity = caret::specificity(data[, "pred"], data[, "obs"])
+  Accuracy = caret::postResample(data[, "pred"], data[, "obs"])[["Accuracy"]]
+  Kappa = caret::postResample(data[, "pred"], data[, "obs"])[["Kappa"]]
+  F1 = caret::F_meas(data[, "pred"], data[, "obs"])
+  Balanced_Accuracy = (Sensitivity + Specificity)/2
+  
+  return(c(
+    Accuracy = Accuracy, 
+    Balanced_Accuracy = Balanced_Accuracy, 
+    Kappa = Kappa,
+    ROC = ROC,
+    Sensitivity = Sensitivity,
+    Specificity = Specificity,
+    F1 = F1
+  ))
 
+}
