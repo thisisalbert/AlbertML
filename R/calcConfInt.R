725a42f61006b8aa0c6da248cc27054266fea1da
Albert Garcia Lopez <albertgarcialopez313@gmail.com>
2023-06-08 13:31:36 +0200

    Update calcConfInt.R

--- R/calcConfInt.R
+++ R/calcConfInt.R
@@ -1,8 +1,12 @@
 calcConfInt <- function(x, perc = 0.95) {
-  x %>%
-    t.test(conf.level = perc) %>%
-    chuck("conf.int") %>%
-    as.numeric() %>%
-    formatC(digits = 3, format = "f") %>%
-    paste(collapse = "-")
+  if (var(x, na.rm = TRUE) == 0) {
+    return(x)
+  } else {
+    x %>%
+      t.test(conf.level = perc, na.action = na.omit) %>%
+      chuck("conf.int") %>%
+      as.numeric() %>%
+      formatC(digits = 3, format = "f") %>%
+      paste(collapse = "-")  
+  }
 }
