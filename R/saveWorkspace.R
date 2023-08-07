c147ace6382e6694b62f2dc81e0b64acb8e401ab
Albert Garcia Lopez <albertgarcialopez313@gmail.com>
2023-08-07 11:14:16 +0200

    Update saveWorkspace.R

--- R/saveWorkspace.R
+++ R/saveWorkspace.R
@@ -1,24 +1,25 @@
-saveWorkspace <- function(workdir = outDir, nameImage = "workspace", saveEnv = TRUE, saveLog = TRUE) {
-  
-  if (exists("workdir") == TRUE) {
-    destination <- workdir
-  } else {
-    destination <- getwd()
+saveWorkspace <- 
+  function(workdir = outDir, nameImage = "workspace", saveEnv = TRUE, saveInfo = TRUE) {
+    
+    if (exists("workdir") == TRUE) {
+      destination = workdir
+    } else {
+      destination = getwd()
+    }
+    
+    if (saveEnv) {
+      save.image(
+        file.path(destination, paste0(format(Sys.Date(), "%y_%m_%d_"), nameImage, ".RData"))
+      )
+    }
+    
+    if (saveInfo) {
+      writeLines(
+        text = capture.output(sessionInfo()), 
+        con = file.path(destination, format(Sys.Date(), "%y_%m_%d_sessionInfo.txt"))
+      )
+    }
+    
+    message("\nWorkspace has been saved!\n")  
+    
   }
-  
-  if (saveEnv) {
-    save.image(
-      file.path(destination, paste0(format(Sys.Date(), "%y_%m_%d_"), nameImage, ".RData"))
-    )
-  }
-  
-  if (saveInfo) {
-    writeLines(
-      text = capture.output(sessionInfo()), 
-      con = file.path(destination, format(Sys.Date(), "%y_%m_%d_sessionInfo.txt"))
-    )
-  }
-  
-  message("\nWorkspace has been saved!\n")  
-  
-}
